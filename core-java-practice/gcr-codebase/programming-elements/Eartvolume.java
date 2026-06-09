00f0982901ee88db18f1e5984a090ddedf715882
Archit Gupta <guptaarchit12345@gmail.com>
2026-06-09 10:08:28 +0530

    [Archit Gupta].add Added repository structure

--- core-java-practice/gcr-codebase/programming-elements/Eartvolume.java
+++ core-java-practice/gcr-codebase/programming-elements/Eartvolume.java
@@ -1,6 +1,6 @@
 public class Eartvolume {
     public static void main(String[] args) {
-        double radius = 6371; // Earth's radius in kilometers
+        double radius = 6371; 
         double volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
         System.out.println("The volume of the Earth is approximately " + volume + " cubic kilometers.");
     }
