3abeec93f96b99e60ebf92d979a4f7e61cd6e6fe
Archit Gupta <guptaarchit12345@gmail.com>
2026-07-19 17:10:54 +0530

    [Archit Gupta].add Hashing  Questions

---
+++ Hashing/FraudPairDetection.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
+
+public class FraudPairDetection {
+    public static int[] findFraudPair(int[] amounts, int target) {
+        Map<Integer, Integer> seen = new HashMap<>(); // amount -> index
+        for (int i = 0; i < amounts.length; i++) {
+            int complement = target - amounts[i];
+            if (seen.containsKey(complement)) {
+                return new int[]{seen.get(complement), i};
+            }
+            seen.put(amounts[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] amounts = {200, 150, 400, 50, 100};
+        int target = 450;
+        int[] result = findFraudPair(amounts, target);
+        System.out.println("Fraud pair indices: " + Arrays.toString(result));
+    }
+}
